955f6d17366fd01c639966f8df49a028517e713e
armend.gashi23@hotmail.com <armend.gashi23@hotmail.com>
2016-02-22 15:50:28 +0100

    Move Player. Jump player fix move in air.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,27 +4,52 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     private Rigidbody pRdb;
+    private CharacterController cCont;
+    private bool grounded = false;
+    private float speed = 6.0F;
+    private float jumpSpeed = 8.0F;
+    private float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         pRdb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
-	
-	}
+        cCont = GetComponent<CharacterController>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(h, 0, v);
+        if (cCont.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButtonDown("Jump"))
+            {   
+                moveDirection.y = jumpSpeed;  
+            }
+
+        }
+        if (!cCont.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal") * 6f, moveDirection.y, Input.GetAxis("Vertical") * 6f);
+
+        }
+            moveDirection.y -= gravity * Time.deltaTime;
+        cCont.Move(moveDirection * Time.deltaTime);
 
 
-        Vector3 vel = pRdb.velocity;
-        vel = move.normalized;
-        pRdb.velocity = vel * 2f;
-   
+    }
 
-	
-	}
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }   
+      
+    }
 }
