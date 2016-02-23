32103d3d61f01c6a3da02c353d07ca3356067fac
unknown <skattan@3se-spel-10.utb.fuv.se>
2016-02-23 10:27:08 +0100

    Movement/camera

---
+++ 3rd Person Cameras/Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public float mSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * mSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * mSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * mSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * mSpeed * Time.deltaTime);
+        }
+
+    }
+}
