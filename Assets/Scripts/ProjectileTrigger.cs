6b038108108fc4d0b57d539176688681a9db052f
unknown <lidean@3se-spel-04.utb.fuv.se>
2016-02-24 15:55:23 +0100

    Added a projectile trigger box that disables all other trigger boxes when triggered.

---
+++ Assets/Scripts/ProjectileTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileTrigger : MonoBehaviour {
+
+    ProjectileTrigger[] mTig;
+    [SerializeField]
+    private bool mTriggered = false;
+
+    void Start()
+    {
+        var triggers = GameObject.FindGameObjectsWithTag("Trigger");// ;.GetComponent<ProjectileTrigger>();
+        mTig = new ProjectileTrigger[triggers.Length];
+        for(int i = 0; i < triggers.Length; i++)
+        {
+            mTig[i] = triggers[i].GetComponent<ProjectileTrigger>();
+        }
+    }
+
+	//Gets triggered when a fireball projectile collides.
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.tag == "Fireball")
+        {
+            foreach(ProjectileTrigger trigger in mTig)
+            {
+                trigger.mTriggered = false;
+            }
+            mTriggered = true;
+        }
+
+    }
+
+}
