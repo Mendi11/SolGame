3658f9eab705ae877440faa709a9e0292c81cc99
Armend Gashi <armend.gashi23@hotmail.com>
2016-03-10 18:13:23 +0100

    Halvt fungerande kod. Fungerar men en platfrom i gamet. bara en.

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -40,21 +40,19 @@ public class PlayerMovement : MonoBehaviour {
     void Start ()
     {
         mFireBallType[0] = true;
-       // mFireball = GameObject.FindGameObjectWithTag("FireballB").GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //if (mFireball == null)
-        //            return; 
 
     }
 
     void Update () {
 
         CursorHide();
-        print(mBallE);
+       
     }
     void LateUpdate()
     {
