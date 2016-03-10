1c1893c030c2f230ca229d0582d2733f948ae49a
unknown <larsan@3se-spel-03.utb.fuv.se>
2016-03-10 16:00:37 +0100

    LEVEL1 updated to ver.3 new material (white color) LightSwitch script added (not working atm)

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private Light mLight;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mLight = GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            mLight.enabled = false;
+        }
+    }
+
+}
