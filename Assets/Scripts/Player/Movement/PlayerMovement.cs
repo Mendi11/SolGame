96f92728d80fc72b287bc0a53c3833e5d3900309
antowam <k8asnakeload@gmail.com>
2016-03-14 22:39:34 +0100

    testat animationer, städat

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -7,14 +7,14 @@ public class PlayerMovement : MonoBehaviour {
     public float mSpeed;
     public float mJumpZ;
 
-
-
     [SerializeField]
     private Transform mCanvas;
     [SerializeField]
     private Rigidbody mFireball;
     [SerializeField]
     private Rigidbody mFireballB;
+
+    public Animator mAnim;
 
     private Transform mPivot;
     private Transform mTarget;
@@ -29,200 +29,300 @@ public class PlayerMovement : MonoBehaviour {
 
 
 
-    // Use this for initialization
+
     void Awake()
-    {
-        mRgb = GetComponent<Rigidbody>();
-        mPivot = GameObject.FindGameObjectWithTag("CameraPivot").GetComponent<Transform>();
-        mTarget = GameObject.FindGameObjectWithTag("CameraTarget").GetComponent<Transform>();
-    }
+        {
+            mRgb = GetComponent<Rigidbody>();
+            mPivot = GameObject.FindGameObjectWithTag("CameraPivot").GetComponent<Transform>();
+            mTarget = GameObject.FindGameObjectWithTag("CameraTarget").GetComponent<Transform>();
+
+            mAnim = GetComponent<Animator>();
+        }
+
 
     void Start ()
-    {
-        mFireBallType[0] = true;
-
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-
-    }
-
-    void Update () {
-
-        CursorHide();
-       
-    }
-    void LateUpdate()
-    {
-
-
-        
-        // The player is able to move using the WASD keys
-      
-        if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * mSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * mSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * mSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * mSpeed * Time.deltaTime);
-        }
-        
 
-        // The player is able to jump by pressing [Space]
-
-        if (Input.GetKeyDown(KeyCode.Space) && mGrounded > 0)
-        {
-            //Hoppar upp med force.
-            mRgb.AddForce(Vector3.up * mJumpZ);
-        }
-        if (Input.GetKey(KeyCode.Alpha1))
-        {
             mFireBallType[0] = true;
 
         }
-        if (Input.GetKey(KeyCode.Alpha2))
+
+
+    void FixedUpdate()
         {
-            mFireBallType[1] = true;
 
         }
-        if (Input.GetKey(KeyCode.E))
-        {
-            if (bulletClone == null)
-                return;
-            transform.position = bulletClone.position;
-            mDestroyFB = true;
 
-        }
-        if (Input.GetKeyDown(KeyCode.Mouse0) && mBallE == true)
-        {
-            mDestroyFB = true;
-           
 
-        }
-        if (Input.GetKey(KeyCode.Mouse1))
-        {            
-            //Canvas ska vara ture om man håller högers mus knapp
-            mCanvas.gameObject.SetActive(true);
-           
-        }
+    void Update ()
+        {
 
-        else
-        {
-            //Canvas ska vara false om man inte håller höger mus knapp
-            mCanvas.gameObject.SetActive(false);
-            //mBallE = false;
-        }
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            mBallE = true;
-            if (bulletClone != null)
-            { return; }
-            else
-            {
-                RaycastB();
-            }
-        }
-
-    }
-    void OnCollisionEnter(Collision col)
-    {
-        if(col.gameObject.tag == "Ground" || col.gameObject.tag == "Platform")
-        {
-            mGrounded += 1;
-        }        
-    }
-    void OnCollisionExit(Collision col)
-    {
-        if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Platform")
-        {
-            mGrounded -= 1;
-        }
-
-    }
-    void Bullet(float speed, RaycastHit hit, Rigidbody fire)
-    {
-
-        //Raycast hittar vad den kolliderar med hämtar positionen den kolliderar med.
-        //Skjuter mot positionen. Skapar 
-        Vector3 shootDirection = hit.point - transform.position;
-        Vector3 move = shootDirection.normalized; 
-        bulletClone = (Rigidbody)Instantiate(fire, transform.position, transform.rotation);
-        Physics.IgnoreCollision(bulletClone.GetComponent<Collider>(), GetComponent<Collider>());
-        bulletClone.velocity = (move * speed) + (Vector3.up * 4);
-
-    }
-    void CursorHide()
-    {
-        // Gömmer eller visar musen och låser den i skärm rutan.
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = (false);
-        }
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = (true);
+            CursorHide();
+       
         }
 
 
-    }
-    void RaycastB()
-    {
-        // Skapar en raycast som kollar vilken direction raycasten ska träffa.
-        RaycastHit hit;
-        Vector3 direction = mTarget.position - mPivot.position;
-        Ray rayLaser = new Ray(mPivot.position, direction);
-        Debug.DrawRay(mPivot.position, direction * 1000);
-        if (Physics.Raycast(rayLaser, out hit, 1000))
+    void LateUpdate()
         {
-            if (hit.collider.tag == "Ground" || hit.collider.tag == "Wall" || hit.collider.tag == "Trigger")
-            {
-                if(mFireBallType[0] == true)
-                    Bullet(7, hit,mFireball);
-                if (mFireBallType[1] == true)
-                    Bullet(7, hit, mFireballB);
-                else
+
+
+/*
+                // The player is able to move using the WASD keys (animated)
+
+            if (Input.GetKey(KeyCode.W))
                 {
-                    //Bullet(15, hit, mFireball);
+                    transform.Translate(Vector3.forward * mSpeed * Time.deltaTime);
+
+                    mAnim.Play("standing_run_forward");
                 }
+
+            else if (Input.GetKey(KeyCode.S))
+                {
+                    transform.Translate(Vector3.back * mSpeed * Time.deltaTime);
+
+                    mAnim.Play("standing_run_back");
+                }
+
+            else if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Translate(Vector3.left * mSpeed * Time.deltaTime);
+
+                    mAnim.Play("standing_run_left");
+                }
+
+            else if (Input.GetKey(KeyCode.D))
+                {
+                    transform.Translate(Vector3.right * mSpeed * Time.deltaTime);
+
+                    mAnim.Play("standing_run_right");
+                }
+
+            else
+                {
+                    mAnim.CrossFade("standing_idle", 0.2f, -1);
+                }
+
+
+
+*/
+                // The player is able to move using the WASD keys (non-animated)
+
+            if (Input.GetKey(KeyCode.W))
+                {
+                    transform.Translate(Vector3.forward * mSpeed * Time.deltaTime);
+                }
+
+            if (Input.GetKey(KeyCode.S))
+                {
+                    transform.Translate(Vector3.back * mSpeed * Time.deltaTime);
+                }
+
+            if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Translate(Vector3.left * mSpeed * Time.deltaTime);
+                }
+
+            if (Input.GetKey(KeyCode.D))
+                {
+                    transform.Translate(Vector3.right * mSpeed * Time.deltaTime);
+                }
+
+
+
+
+
+            // The player is able to jump by pressing [Space]
+
+        if (Input.GetKeyDown(KeyCode.Space) && mGrounded > 0)
+            {
+                //Hoppar upp med force.
+
+                mRgb.AddForce(Vector3.up * mJumpZ);
             }
+
+
+
+                // Switch between fireball colors
+
+            if (Input.GetKey(KeyCode.Alpha1))
+                {
+                    mFireBallType[0] = true;
+                }
+
+            if (Input.GetKey(KeyCode.Alpha2))
+                {
+                    mFireBallType[1] = true;
+                }
+
+
+
+                // If E is pressed, teleport player to current fireball
+
+            if (Input.GetKey(KeyCode.E))
+                {
+                    if (bulletClone == null)
+                        return;
+
+                    transform.position = bulletClone.position;
+                    mDestroyFB = true;
+                }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0) && mBallE == true)
+                {
+                    mDestroyFB = true;
+                }
+
+
+
+                // Activates/deactivates aim overlay by holding Mouse 2
+
+            if (Input.GetKey(KeyCode.Mouse1))
+                {            
+                
+                    mCanvas.gameObject.SetActive(true);
+                }
+
+            else
+                {
+
+                    mCanvas.gameObject.SetActive(false);
+
+                    //mBallE = false;
+
+                }
+
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+                {
+                    mBallE = true;
+
+                        if (bulletClone != null)
+
+                            { return; }
+
+                                else
+                                    {
+                                        RaycastB();
+                                    }
+                }
+    }
+
+
+    
+
+
+
+        // Check if Player.tag is colliding with ground or platform
+
+    void OnCollisionEnter(Collision col)
+        {
+            if(col.gameObject.tag == "Ground" || col.gameObject.tag == "Platform")
+                {
+                    mGrounded += 1;
+                }        
         }
 
-    }
+    void OnCollisionExit(Collision col)
+        {
+            if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Platform")
+                {
+                    mGrounded -= 1;
+                }
+
+        }
+
+
+
+
+
+    void Bullet(float speed, RaycastHit hit, Rigidbody fire)
+        {
+
+            //Raycast hittar vad den kolliderar med hämtar positionen den kolliderar med.
+            //Skjuter mot positionen. Skapar 
+            Vector3 shootDirection = hit.point - transform.position;
+            Vector3 move = shootDirection.normalized; 
+            bulletClone = (Rigidbody)Instantiate(fire, transform.position, transform.rotation);
+            Physics.IgnoreCollision(bulletClone.GetComponent<Collider>(), GetComponent<Collider>());
+            bulletClone.velocity = (move * speed) + (Vector3.up * 4);
+
+        }
+
+
+    void CursorHide()
+        {
+
+
+            // Gömmer eller visar musen och låser den i skärm rutan.
+
+            if (Input.GetKeyDown(KeyCode.C))
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = (false);
+                }
+            if (Input.GetKeyDown(KeyCode.V))
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = (true);
+                }
+        }
+
+
+    void RaycastB()
+        {
+        
+        
+            // Skapar en raycast som kollar vilken direction raycasten ska träffa.
+
+            RaycastHit hit;
+            Vector3 direction = mTarget.position - mPivot.position;
+            Ray rayLaser = new Ray(mPivot.position, direction);
+            Debug.DrawRay(mPivot.position, direction * 1000);
+
+            if (Physics.Raycast(rayLaser, out hit, 1000))
+            {
+                if (hit.collider.tag == "Ground" || hit.collider.tag == "Wall" || hit.collider.tag == "Trigger")
+                    {
+                        if(mFireBallType[0] == true)
+                            Bullet(7, hit,mFireball);
+
+                        if (mFireBallType[1] == true)
+                            Bullet(7, hit, mFireballB);
+
+                        else
+                            {
+                                //Bullet(15, hit, mFireball);
+                            }
+                    }
+            }
+
+        }
+
+
+
 
     void RestFireBall()
-    {
-        for (int i = 0; i < mFireBallType.Length; i++)
         {
-            mFireBallType[i] = false;
+            for (int i = 0; i < mFireBallType.Length; i++)
+                {
+                    mFireBallType[i] = false;
+                }
         }
-    }
 
     public bool DestroyFB
-    {
-       get { return mDestroyFB; }
-       set { mDestroyFB = value; }
-    }
+        {
+           get { return mDestroyFB; }
+           set { mDestroyFB = value; }
+        }
+
     public bool DestroyB
-    {
-        get { return mBallE; }
-        set { mBallE = value; }
-    }
+        {
+            get { return mBallE; }
+            set { mBallE = value; }
+        }
+
     public bool[] FireBallType
-    {
-        get { return mFireBallType; }
-        set { mFireBallType = value; }
+        {
+            get { return mFireBallType; }
+            set { mFireBallType = value; }
+        }
     }
-}
