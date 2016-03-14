2360c94098631970d196e555cbe67f157c0f5c96
unknown <gashar@3se-spel-05.utb.fuv.se>
2016-03-14 17:28:57 +0100

    Snyggat till koden

--- Assets/Scripts/Plattform/PlattMovement.cs
+++ Assets/Scripts/Plattform/PlattMovement.cs
@@ -6,17 +6,17 @@ public class PlattMovement : MonoBehaviour {
 
     private Rigidbody mPlattRgb;
     [SerializeField]
-    private float mPosX = 0;
+    private float mDirSpeedX = 0;
     [SerializeField]
-    private float mPosY = 0;
+    private float mDirSpeedY = 0;
     [SerializeField]
-    private float mPosZ = 0;
+    private float mDirSpeedZ = 0;
     [SerializeField]
-    private float mEPosX = 0;
+    private float mEndPosX = 0;
     [SerializeField]
-    private float mEPosY = 0;
+    private float mEndPosY = 0;
     [SerializeField]
-    private float mEPosZ = 0;
+    private float mEndPosZ = 0;
     [SerializeField]
     private bool mOn = true;
     [SerializeField]
@@ -26,69 +26,42 @@ public class PlattMovement : MonoBehaviour {
 
     Vector3 mMove;
     Vector3 mStartPos;
-    private bool mDownUp = true;
-    private bool mLeftRight = true;
-    private bool mFowardBack = true;
+    private bool mDownUpDir = true;
+    private bool mLeftRightDir = true;
+    private bool mFowardBackDir = true;
     // Use this for initialization
-    void Start ()
+  
+    void Start()
     {
         mPlattRgb = GetComponent<Rigidbody>();
         mStartPos = mPlattRgb.position;
         mPlattRgb.position += new Vector3(1, 1, 1);
+        // Move po
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-       
-        if (mPlattRgb.position.y >= mEPosY && mDownUp == true)
-        {
-             mPosY = mPosY * -1;
-             mDownUp = false;
-        }
-        else if (mPlattRgb.position.y <= (mStartPos.y - 0.5) && mDownUp == false)
-        {
-            mPosY = mPosY * -1;
-            mDownUp = true;
-
-        }
-        if (mPlattRgb.position.x >= mEPosX && mLeftRight == true)
-        {
-            mPosX = mPosX * -1;
-            mLeftRight = false;
-        }
-        else if (mPlattRgb.position.x <= (mStartPos.x - 0.5) && mLeftRight == false)
-        {
-            mPosX = mPosX * -1;
-            mLeftRight = true;
-
-        }
-        if (mPlattRgb.position.z >= mEPosZ && mFowardBack == true)
-        {
-            mPosZ = mPosZ * -1;
-            mFowardBack = false;
-        }
-        else if (mPlattRgb.position.z <= (mStartPos.z - 0.5) && mFowardBack == false)
-        {
-            mPosZ = mPosZ * -1;
-            mFowardBack = true;
-        }
-
-
-
+        //Get wich direction it should move.
+        mDirSpeedX = Platform(mEndPosX, mDirSpeedX, mLeftRightDir, mPlattRgb.position.x, mStartPos.x,"x");
+        mDirSpeedY = Platform(mEndPosY, mDirSpeedY, mDownUpDir, mPlattRgb.position.y, mStartPos.y, "y");
+        mDirSpeedZ = Platform(mEndPosZ, mDirSpeedZ, mFowardBackDir, mPlattRgb.position.z, mStartPos.z,"z");
         if (mOn == true)
         {
-            mMove = new Vector3(mPosX, mPosY, mPosZ);
+            //if mOn is true.Then its should move at the direction it has.
+            mMove = new Vector3(mDirSpeedX, mDirSpeedY, mDirSpeedZ);
             mPlattRgb.velocity = mMove;
         }
         else if (mOn == false)
         {
+            //if mOn is false Then its should stay still.
             mMove = new Vector3(0, 0, 0);
             mPlattRgb.velocity = mMove;
 
         }
     }
 
+    //Get and setters.
     public bool PlattOn
     {
         get { return mOn; }
@@ -99,6 +72,44 @@ public class PlattMovement : MonoBehaviour {
     {
         get { return mPlatID; }
         set { mPlatID = value; }
+
+    }
+
+    float Platform(float maxPos , float speedDir, bool dir, float platform,float startPos,string xyz)
+    {
+        //Get the platforms position and check it if its greater than maxPos.
+        if (platform >= maxPos && dir == true)
+        {
+            // Make the direction negetiv or positiv
+            speedDir = speedDir * -1;
+            dir = false;
+        }
+        else if (platform <= (startPos - 0.5) && dir == false)
+        {
+            // Make the direction negetiv or positiv
+            speedDir = speedDir * -1;
+            dir = true;
+        }
+        Direction(xyz,dir);
+
+        return speedDir;
+    }
+    void Direction(string xyz,bool dir)
+        {
+        if (xyz == "x")
+        {
+            mLeftRightDir = dir;
+            
+        }
+        else if (xyz == "y")
+        {
+            mDownUpDir = dir;
+        }
+        else if (xyz == "z")
+        {
+            mFowardBackDir = dir;
+
+        }
 
     }
 }
