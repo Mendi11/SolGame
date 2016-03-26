8b6984e20154ec43ff5921f84f1508ccfa2fbc4f
unknown <gashar@3se-spel-05.utb.fuv.se>
2016-03-26 14:41:21 +0100

    Fixed fireball resets when restarting the game. Fixed mainmenu to have its own gameobject

--- Assets/Scripts/World/MainMenu.cs
+++ Assets/Scripts/World/MainMenu.cs
@@ -4,13 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    GameController mGameController;
+
+
     public void ClickedStart()
     {
+        mGameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
         StartCoroutine(StartGame());
     }
 
     IEnumerator StartGame()
     {
+
+        mGameController.BallActive = false;
         float mFadeTime = GameObject.Find("GameController").GetComponent<FadeScene>().BeginFade(1);
         yield return new WaitForSeconds(mFadeTime);
         SceneManager.LoadScene(1);
