07ac5ab5be457f89c13368537f514a83c71a75c6
armend.gashi23@hotmail.com <armend.gashi23@hotmail.com>
2016-03-29 19:21:34 +0200

    Fixed Screenshake on triggerball. Fixed player pivot rotate player.

---
+++ Assets/Scripts/Player/Camera/CameraShake.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+
+    
+    Transform mPivot;
+
+    bool mResetPos = false;
+    bool mIsShakeOn = false;
+    Vector3 moriginalCameraPosition;
+    float mshakeAmt = 0;
+    float mTimer = 3;
+    //private Camera mmainCamera;
+
+    void Awake()
+    {
+        mPivot = GameObject.FindGameObjectWithTag("CameraPivot").transform;
+        //moriginalCameraPosition = mPivot.position;
+
+        //mmainCamera = Camera.main;
+        mshakeAmt = 0.05f;
+        InvokeRepeating("CameeraShake", 0, 0.01f);
+        //Invoke("StopShaking", 0.001f);
+       
+
+    }
+
+    void CameeraShake()
+    {
+
+        if (mshakeAmt > 0 && mIsShakeOn == true)
+        {
+            float quakeAmt = Random.value * mshakeAmt * 2 - mshakeAmt;
+            Vector3 pp = mPivot.position;
+            pp.y += quakeAmt; // can also add to x and/or z
+            pp.x += quakeAmt;
+            mPivot.position = pp;
+            if (mTimer <= 0.5f)
+            {
+                mIsShakeOn = false;
+                mResetPos = true;
+            }
+        }
+    }
+
+    void Update()
+    {
+        mTimer -= Time.deltaTime;
+        if (mResetPos == true)
+        {
+            mPivot.localPosition = new Vector3(-0.07f, 1.35f, -13f);
+            mResetPos = false;
+
+        }
+    }
+    void StopShaking()
+    {
+        
+        CancelInvoke("CameeraShake");
+       // mPivot.position = moriginalCameraPosition;
+        //mIsShakeOn = false;
+
+    }
+
+    public bool ShakCamera
+    {
+        get { return mIsShakeOn; }
+        set { mIsShakeOn = value; }
+
+    }
+    public float Timer
+    {
+        get { return mTimer; }
+        set { mTimer = value; }
+
+    }
+
+}
